999e32b248f452f0dafcbe27b5aa07a81918dbbf
Malte Franken <exxamalte@users.noreply.github.com>
2023-01-25 08:38:23 +1100

    Improve test coverage (#4)

---
+++ tests/test_init.py
@@ -0,0 +1,7 @@
+"""Test for the USGS Earthquake Hazards Program feed general setup."""
+from aio_geojson_usgs_earthquakes import __version__
+
+
+def test_version():
+    """Test for version tag."""
+    assert __version__ is not None
