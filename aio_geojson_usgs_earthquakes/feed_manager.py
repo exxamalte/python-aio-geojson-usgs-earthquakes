8339fd23fbc65a4c9e80d88555ca5eb4fc5d54af
Malte Franken <exxamalte@users.noreply.github.com>
2024-01-26 22:58:42 +1100

    fix backwards compatibility

--- aio_geojson_usgs_earthquakes/feed_manager.py
+++ aio_geojson_usgs_earthquakes/feed_manager.py
@@ -1,4 +1,6 @@
 """Feed Manager for USGS Earthquake Hazards Program feed."""
+from __future__ import annotations
+
 from collections.abc import Awaitable, Callable
 
 from aio_geojson_client.feed_manager import FeedManagerBase
