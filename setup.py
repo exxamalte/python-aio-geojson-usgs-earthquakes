c00fa4b7750ab55d10c04faeb09a569e43ee17bb
Malte Franken <exxamalte@users.noreply.github.com>
2024-01-26 23:15:23 +1100

    add python 3.12 support

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+"""Setup of aio_geojson_usgs_earthquakes library."""
 import os
 
 from setuptools import find_packages, setup
@@ -15,7 +16,7 @@ REQUIRES = [
 ]
 
 
-with open("README.md", "r") as fh:
+with open("README.md") as fh:
     long_description = fh.read()
 
 HERE = os.path.abspath(os.path.dirname(__file__))
@@ -38,6 +39,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
