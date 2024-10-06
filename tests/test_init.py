a3d0c786bad15baa020deb00a34a0dbbbd733f42
Malte Franken <exxamalte@users.noreply.github.com>
2024-10-06 23:43:42 +1100

    Migrate to pyproject.toml (#19)
    * migrate to pyproject.toml
    
    * remove tests from source dist

--- tests/test_init.py
+++
@@ -1,8 +0,0 @@
-"""Test for the USGS Earthquake Hazards Program feed general setup."""
-
-from aio_geojson_usgs_earthquakes import __version__
-
-
-def test_version():
-    """Test for version tag."""
-    assert __version__ is not None
